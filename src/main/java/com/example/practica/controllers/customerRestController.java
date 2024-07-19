52255f48b9e4d9f4e702892d0c8a3a1277dcdbac
Folynsky <kevin.foly06@gmail.com>
2024-07-18 19:03:31 -0600

    endpoint deleteMapping

--- src/main/java/com/example/practica/controllers/customerRestController.java
+++ src/main/java/com/example/practica/controllers/customerRestController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,27 +12,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.practica.model.customer;
+
 @RestController
 public class customerRestController {
 
-
-
     private List<customer> customers = new ArrayList<>(Arrays.asList(
 
-        new customer(1,"John Doe", "Jhondu", "1234"),
-        new customer(2,"Jane Smith", "Alex Perrez", "34534"),
-        new customer(3,"Jane Smith", "Alex Perrez", "34534"),
-        new customer(4,"Jane Smith", "Alex Perrez", "34534")
-    ));
-    
+            new customer(1, "John Doe", "Jhondu", "1234"),
+            new customer(2, "Jane Smith", "Alex Perrez", "34534"),
+            new customer(3, "Jane Smith", "Alex Perrez", "34534"),
+            new customer(4, "Jane Smith", "Alex Perrez", "34534")));
+
     @GetMapping("/clientes")
     public List<customer> getCustomers() {
         return customers;
     }
+
     @GetMapping("/clientes/{username}")
-    public customer getCustomer(@PathVariable String username){  
-        for(customer c : customers){
-            if (c.getUsername().equalsIgnoreCase(username)){ 
+    public customer getCustomer(@PathVariable String username) {
+        for (customer c : customers) {
+            if (c.getUsername().equalsIgnoreCase(username)) {
                 return c;
             }
         }
@@ -39,8 +39,20 @@ public class customerRestController {
     }
 
     @PostMapping("/clientes/post")
-    public customer postCustomer(@RequestBody customer customer){
+    public customer postCustomer(@RequestBody customer customer) {
         customers.add(customer);
         return customer;
+    }
+
+    @DeleteMapping("/clientes/delete/{id}")
+    public customer deleteCustomer(@PathVariable int id) {
+        for (customer c : customers) {
+            if (c.getId() == id) {
+                customers.remove(c);
+
+                return c;
+            }
+        }
+        return null;
     }
 }
